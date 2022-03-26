522beb28ddf6b68337cc10271cbf92dfda7348ec
elmurie <elmurie@gmail.com>
2022-03-26 19:26:09 +0100

    behaviour completed

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const RANDOM_QUOTE_URL = 'https://api.quotable.io/RANDOM';
-const keycaps = document.querySelectorAll('[key-value]');
-
+const timerDisplay = document.querySelector('[timer]');
 const quoteDisplayContent = document.querySelector('[quote-display-content]');
 const quoteDisplayAuthor = document.querySelector('[quote-display-author]');
 const userInput = document.querySelector('[quote-input]');
+const keycaps = document.querySelectorAll('[key-value]');
+const roundNumberDisplay = document.querySelector('[round-number]');
+const pointsDisplay = document.querySelector('[points]');
 
-
+// API CALL TO GET THE QUOTE
 function getQuote() {
     return fetch(RANDOM_QUOTE_URL)
-    .then((response) => response.json())
+        .then((response) => response.json())
 }
 
+// RENDER OF QUOTE
 async function getNewQuote() {
     const quote = await getQuote();
     quoteDisplayContent.innerHTML = '';
@@ -21,8 +24,10 @@ async function getNewQuote() {
         quoteDisplayContent.appendChild(characterSpan);
     });
     userInput.value = '';
+    startTimer();
 }
 
+// LISTENER KEYBOARD ANIMATION
 window.addEventListener('keydown', (key) => {
     keycaps.forEach((keycap) => {
         if (keycap.innerHTML.toUpperCase() === key.key.toUpperCase()) {
@@ -34,17 +39,23 @@ window.addEventListener('keydown', (key) => {
     })
 });
 
+// LISTENER TEXTAREA
 userInput.addEventListener('input', () => {
+    // PASTE DISABLED ON TEXTAREA
+    userInput.onpaste = e => {
+        e.preventDefault();
+        return false;
+    };
     let correct = true;
     const arrayQuote = quoteDisplayContent.querySelectorAll('span');
     const arrayValue = userInput.value.split('');
     arrayQuote.forEach((characterSpan, index) => {
         const character = arrayValue[index];
-        if ( character == null) {
+        if (character == null) {
             characterSpan.classList.remove('correct');
             characterSpan.classList.remove('wrong');
             correct = false;
-        } else if ( character === characterSpan.innerText) {
+        } else if (character === characterSpan.innerText) {
             characterSpan.classList.add('correct');
             characterSpan.classList.remove('wrong');
         } else {
@@ -53,8 +64,42 @@ userInput.addEventListener('input', () => {
             correct = false;
         }
     })
-    console.log(correct);
-    if (correct) getNewQuote();
+    if (correct) {
+        getNewQuote();
+        getRoundNumber();
+        getPoints(arrayQuote.length, timerDisplay.innerHTML);
+    }
 })
+
+// TIMER FUNCTIONS
+let startTime;
+function startTimer() {
+    timerDisplay.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timerDisplay.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
+}
+
+// UPDATES ROUND NUMBER AFTER QUOTE IS COMPLETED AND CORRECT 
+
+function getRoundNumber() {
+    let number = roundNumberDisplay.innerHTML;
+    number++;
+    roundNumberDisplay.innerHTML = number;
+
+}
+
+// UPDATES SCORE
+
+function getPoints(quoteLength, time) {
+    let currentPoints = parseInt(pointsDisplay.innerHTML);
+    currentPoints += quoteLength - Math.round( time / 2 );
+    pointsDisplay.innerHTML = currentPoints;
+}
 
 getNewQuote();
